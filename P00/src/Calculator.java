4b1a2421463c02c929daae87caabf0a3747c9318
justinPhua <21033243@myrp.edu.sg>
2022-05-30 13:06:11 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,27 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21033243, 30 May 2022 9:40:57 am
+ */
+
+public class Calculator {
+
+		public int add (int a, int b) {
+			 return a + b;
+			}
+		
+		public int substract (int a, int b) {
+			return a - b;
+		}
+		
+		public int multiple (int a, int b) {
+			return a * b;
+		}
+		
+		public int devide (int a, int b) {
+			return a / b;
+		}
+
+	}
---
+++ P00/test/CalculateTest.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculateTest {
+	
+private static final int ArithmeticException = 0;
+int a,b;
+Calculator cal;
+
+@Before
+public void setUp() throws Exception {
+	a = 4321;
+	b = 1234;
+	cal = new Calculator();
+	}
+
+	@Test
+	public void testAdd() {
+		int actual = cal.add(a, b); 
+		int expected = 5555;
+		assertEquals (expected, actual);
+		}
+
+	@Test
+	public void testSubstract() {
+		int actual = cal.substract(a, b); 
+		int expected = 3087;
+		assertEquals (expected, actual);
+		}
+	
+	@Test
+	public void testMultiple() {
+		int actual = cal.multiple(a, b);  
+		int expected = 5332114;
+		assertEquals (expected, actual);
+		}
+	
+	@Test
+	public void testDevide() {
+		int e = 10;
+		int f = 2;
+		int actual = cal.devide(e, f); 
+		int expected = 5;
+		assertEquals (expected, actual);
+		}
+
+	@Test
+	public void testDevideError() {
+		int c = 3;
+		int d = 0;
+		
+		//Act
+		try{
+		    cal.devide(c, d);
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e){
+		    assertEquals("cannot be devided by zero", e.getMessage());
+		} catch (Throwable t) {
+			assertEquals ("Expected an IllegalArgumentException to be thrown", t.getMessage());
+		}
+
+		}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+}
